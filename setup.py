398f34133e533d66b35902ffd8162ea339cbbd2d
BumpVersion Action <bumpversion@github-actions>
2024-11-20 17:34:39 +0000

    Bump version: 0.0.6 → 0.0.7 [ci skip]

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ install_requires = (this_directory / "requirements.txt").read_text().splitlines(
 
 setup(
     name="solar_and_storage",
-    version="0.0.6",
+    version="0.0.7",
     license="MIT",
     description="Modelling for Solar and Storage",
     author="Peter Dudfield",
